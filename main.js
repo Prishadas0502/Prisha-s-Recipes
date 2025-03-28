63140d9ac65d77ccf88937800a18b730c836e134
Prisha Das <prishadas000@gmail.com>
2025-03-28 17:38:05 +0530

    jhkh

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ toggle.addEventListener('click',function(){
 })
 
 
+// Search
+document.getElementById("searchIcon").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    let searchBar = document.getElementById("searchBar");
+    
+    // Toggle the search bar visibility
+    if (searchBar.classList.contains("hidden")) {
+        searchBar.classList.remove("hidden");
+        searchBar.focus(); // Automatically focus on the input
+    } else {
+        searchBar.classList.add("hidden");
+    }
+});
+
+function searchPosts() {
+    let input = document.getElementById('searchBar').value.toLowerCase();
+    let posts = document.querySelectorAll('.grid-item');
+
+    posts.forEach(post => {
+        let title = post.querySelector('.text-title').innerText.toLowerCase();
+        post.style.display = title.includes(input) ? 'block' : 'none';
+    });
+}
+
